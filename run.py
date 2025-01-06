4ba21324077eb126c73ec8fd3e65e71eb105e9fc
Giovana S Assis <81191473+giosassis@users.noreply.github.com>
2025-01-06 14:12:12 -0300

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app.database import get_db_connection
 from app.main import app
 
 if __name__ == '__main__':
-    get_db_connection()  # Inicializa a conexão com o banco
+    get_db_connection()  
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
